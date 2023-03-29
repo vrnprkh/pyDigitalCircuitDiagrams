3f110366c54b63b2cc994a269ac844a410c192ea
vrnprkh <paintingburritos@gmail.com>
2023-03-29 12:12:01 -0400

    UpdateReadme
    Fixed full adder example

--- README.md
+++ README.md
@@ -89,10 +89,9 @@ Note: Wires are currently simply drawn as straight lines.
 4-bit Full Adder:
 
 ```
-$(3,0)[{XOR: a&.a&, b&.b& -> fg&}{AND: a&.a&, b&.b& -> fcout&}];
-$(3,0)[{XOR: cin&.cin&, fg& -> sg&^}{AND: cin&.cin&, fg& -> scout&^.output&}];
-$(3,0)[{OR: fcout&, scout& -> cin(&+1).cout&#10}
-]
+$(3,0)[{XOR: a&.a&, b&.b& -> fg&}{AND: a&.a&, b&.b& -> fcout&}]{CIN:NA_->cin0};
+$(3,0)[{XOR: cin&.cin&, fg& ->_,sg&^.output&}{AND: cin&.cin&, fg& -> scout&}];
+$(3,0)[{OR: fcout&, scout& -> cin(&+1).cout&#10}]
 ```
 
 ![](images/sampleImage1.PNG)
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def fullBuild(inputString):
     newBoard.drawBoard()
 
 inputStr = """
-    {CIN:NA_->cin0}$(3,0)[{XOR: a&.a&, b&.b& -> fg&}{AND: a&.a&, b&.b& -> fcout&}];
-    $(3,0)[{XOR: cin&.cin&, fg& -> sg&^}{AND: cin&.cin&, fg& -> scout&^.output&}];
+    $(3,0)[{XOR: a&.a&, b&.b& -> fg&}{AND: a&.a&, b&.b& -> fcout&}]{CIN:NA_->cin0};
+    $(3,0)[{XOR: cin&.cin&, fg& ->_,sg&^.output&}{AND: cin&.cin&, fg& -> scout&}];
     $(3,0)[{OR: fcout&, scout& -> cin(&+1).cout&#10}]
     """
 inputStr2 = """$(0,3)[{CoolBlock&: a&.nameOfA&, b&.nameOfB&->c&.nameOfC&}]"""
